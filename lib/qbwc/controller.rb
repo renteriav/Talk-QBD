6e0b6b8da86d0c3220e406b7b22e7315bd07963d
Francisco Renteria <renteriav@hotmail.com>
2016-08-14 23:10:09 -0700

    update integrations true after first sync

--- lib/qbwc/controller.rb
+++ lib/qbwc/controller.rb
@@ -72,9 +72,7 @@ module QBWC
         qbd_client.username = @username
         qbd_client.user_id = @user_id
       end
-      if qbd_client.save
-        qbd_client.client.update_attributes(integrations: true, integration_software: "qbd")
-        
+      if qbd_client.save        
         import_all_entities(@username, session[:client_id])
            
         scheduler_block = ''
